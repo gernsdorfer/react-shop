4d4d176e6d24b09a0986463aa7bfad796403c33d
gernsdorfer <lars.wiedemann@gmx.net>
2017-02-18 14:51:55 +0100

    use hash history for app

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import store from './store';
 import {Provider} from 'react-redux';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 
 import Market from './containers/market.container';
 import ShelfAdmin from './containers/shelf.admin.container';
@@ -10,7 +10,7 @@ import ProductAdmin from './containers/product.admin.container';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={browserHistory}>
+        <Router history={hashHistory}>
                 <Route path="/" component={Market}/>
                 <Route path="/shelf-admin" component={ShelfAdmin}/>
                 <Route path="/product/edit/:productId" component={ProductAdmin}/>
